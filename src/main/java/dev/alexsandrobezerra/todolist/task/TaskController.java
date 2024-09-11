d422bc3ee8ff4a06896091be08fdb354a6dd49fa
Alexsandro Bezerra <alexsandro.g.bezerra@gmail.com>
2024-09-11 16:47:19 -0300

    list user's tasks

--- src/main/java/dev/alexsandrobezerra/todolist/task/TaskController.java
+++ src/main/java/dev/alexsandrobezerra/todolist/task/TaskController.java
@@ -2,12 +2,10 @@ package dev.alexsandrobezerra.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -20,9 +18,27 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
+    @GetMapping
+    public ResponseEntity list(HttpServletRequest request) {
+        final var userId = UUID.fromString(request.getAttribute("userId").toString());
+        var tasks = this.taskRepository.findByUserId(userId);
+        return ResponseEntity.ok(tasks);
+    }
+
     @PostMapping
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        taskModel.setUserId(UUID.fromString(request.getAttribute("userId").toString()));
+        final var userId = UUID.fromString(request.getAttribute("userId").toString());
+        taskModel.setUserId(userId);
+
+        final var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartedAt()) || currentDate.isAfter(taskModel.getFinishedAt())) {
+            return ResponseEntity.badRequest().body("Data de início/final anterior a data atual");
+        }
+        if (taskModel.getStartedAt().isAfter(taskModel.getFinishedAt())) {
+            return ResponseEntity.badRequest().body("Data final não pode ser anterior a data inicial");
+        }
+
+
         var createdTask = this.taskRepository.save(taskModel);
         var location = String.format("/tasks/%s", createdTask.getId());
         return ResponseEntity.created(URI.create(location)).body(createdTask);
