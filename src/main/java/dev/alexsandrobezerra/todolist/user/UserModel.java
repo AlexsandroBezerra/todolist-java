80abe3e2bc0dc565db4930b62340087714e4510a
Alexsandro Bezerra <alexsandro.g.bezerra@gmail.com>
2024-09-11 08:50:09 -0300

    create and save user in database

--- src/main/java/dev/alexsandrobezerra/todolist/user/UserModel.java
+++ src/main/java/dev/alexsandrobezerra/todolist/user/UserModel.java
@@ -1,14 +1,29 @@
 package dev.alexsandrobezerra.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    private UUID id;
 
     private String username;
 
     private String name;
 
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
---
+++ src/main/java/dev/alexsandrobezerra/todolist/user/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.alexsandrobezerra.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+
+}
