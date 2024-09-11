42f00db293f3f593d4490071eaed02a08961b9c8
Alexsandro Bezerra <alexsandro.g.bezerra@gmail.com>
2024-09-11 07:58:32 -0300

    init project

---
+++ src/test/java/dev/alexsandrobezerra/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexsandrobezerra.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
